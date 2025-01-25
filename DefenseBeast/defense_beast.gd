3d05fdb59b51ee08f2f670f584c22f9682b3651e
Bobby <rhulzing@yahoo.com>
2025-01-25 09:54:17 -0600

    Added test curve, have curve loaded programmatically into defense beast

--- DefenseBeast/defense_beast.gd
+++ DefenseBeast/defense_beast.gd
@@ -2,18 +2,27 @@ extends Node3D
 
 @onready var bubble_animal = $DefaultBehavior/Path3D/PathFollow3D/NeutralBubbleAnimal
 @onready var default_behavior = $DefaultBehavior
+@onready var path = $DefaultBehavior/Path3D
 @onready var follow_3d = $DefaultBehavior/Path3D/PathFollow3D
 @onready var defense_behavior = $DefenseBehavior
+
+@export var default_curve: Curve3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	path.curve = default_curve
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	const move_speed = 4.0
+	if bubble_animal.get_parent() == follow_3d:
+		follow_3d.progress += 4.0 * delta
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
