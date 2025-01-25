3dce610d22fef032fe2e6515a834c082404ce733
Adhan Razzaque <adhan.razzaque@gmail.com>
2025-01-25 14:41:58 -0600

    Add credits starter and make all connections

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 @export_file("*.tscn") var main_scene_path = "res://bubble_world_map.tscn"
+@export_file("*.tscn") var credits_scene_path = "res://credits_screen.tscn"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +15,7 @@ func _on_exit_pressed() -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file(main_scene_path)
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file(credits_scene_path)
