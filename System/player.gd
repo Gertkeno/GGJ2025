02514c41f966e84179b535ecb79554fa9592cd2f
Garrett Hale <gthkeno@gmail.com>
2025-01-25 11:31:14 -0600

    translations and a settings menu

--- System/player.gd
+++ System/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody3D
-
+class_name Player
 
 const SPEED: float = 5.0
 const CROUCH_SPEED: float = 3.5
 const GROUND_ACCELERATION: float = 40.0
 const AIR_ACCELERATION: float = 5.0
 const JUMP_VELOCITY: float = 4.5
+
+#region control options
+static var camera_sensitivity: float = 1.0
+static var y_invert: float = 1.0
+#endregion
+
+
+@onready var hurt_timer: Timer = $HurtTimer
 
 
 func _physics_process(delta: float) -> void:
@@ -16,7 +24,7 @@ func _physics_process(delta: float) -> void:
 	var horizontal_velocity := velocity - vertical_velocity
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and hurt_timer.is_stopped():
 		vertical_velocity = JUMP_VELOCITY * up_direction
 
 	# Get the input direction and handle the movement/deceleration.
@@ -24,9 +32,13 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := transform.basis.orthonormalized() * Vector3(input_dir.x, 0, input_dir.y)
 
-	var accel: float = GROUND_ACCELERATION if is_on_floor() else AIR_ACCELERATION
-	var speed: float = CROUCH_SPEED if Input.is_action_pressed("crouch") else SPEED
-	horizontal_velocity = horizontal_velocity.move_toward(direction*speed, accel*delta)
+	if hurt_timer.is_stopped():
+		var accel: float = GROUND_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+		var speed: float = CROUCH_SPEED if Input.is_action_pressed("crouch") else SPEED
+		horizontal_velocity = horizontal_velocity.move_toward(direction*speed, accel*delta)
+	else:
+		horizontal_velocity = horizontal_velocity.move_toward(Vector3.ZERO, AIR_ACCELERATION*delta)
+		
 	velocity = horizontal_velocity + vertical_velocity
 
 	move_and_slide()
@@ -37,14 +49,32 @@ func _ready() -> void:
 	screen_size = DisplayServer.screen_get_size()
 
 
+func _process(delta: float) -> void:
+	var controller_direction: Vector2 = Input.get_vector("turn_left", "turn_right", "turn_up", "turn_down")
+	if controller_direction:
+		var camera_move: Vector2 = controller_direction * PI * delta * camera_sensitivity
+		rotate_y(-camera_move.x)
+		$CameraPivot.rotate_x(camera_move.y * y_invert)
+		$CameraPivot.rotation.x = clampf($CameraPivot.rotation.x, -CAMERA_ANGLE_MAX, CAMERA_ANGLE_MAX)
+
+
 const CAMERA_ANGLE_MAX = deg_to_rad(70)
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-			rotate_y(-event.screen_relative.x / screen_size.x * PI)
-			$CameraPivot.rotate_x(-event.screen_relative.y / screen_size.y * PI)
+			var camera_move: Vector2 = event.screen_relative / screen_size * -PI * camera_sensitivity
+
+			# TODO: rotate camera around player model
+			rotate_y(camera_move.x)
+			$CameraPivot.rotate_x(camera_move.y * y_invert)
 			$CameraPivot.rotation.x = clampf($CameraPivot.rotation.x, -CAMERA_ANGLE_MAX, CAMERA_ANGLE_MAX)
 	elif event is InputEventMouseButton:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif event.is_action_pressed("menu"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		$Settings.show()
+		get_tree().paused = true
+
+
+func _on_game_settings_continue_pressed() -> void:
+	$Settings.hide()
