edbc4d619db45a3d147a2e4fc6bfb8e08b054fa8
Garrett Hale <gthkeno@gmail.com>
2025-01-25 21:46:55 -0600

    animals look forward

--- SkittishBeast/skittish_behavior.gd
+++ SkittishBeast/skittish_behavior.gd
@@ -8,24 +8,15 @@ var destination: Vector3
 
 signal finished_fleeing
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func process_behavior(delta: float) -> void:
-	
+
 	if is_nav_finished():
 		finished_fleeing.emit()
 	else:
 		bubble_animal.direction = bubble_animal.global_position.direction_to(destination)
 		bubble_animal.look_at(destination)
 		bubble_animal.apply_movement()
-	
+
 func is_nav_finished() -> bool:
 	return bubble_animal.navigation_agent.is_navigation_finished()
 	# return bubble_animal.navigation_agent.is_target_reached()
