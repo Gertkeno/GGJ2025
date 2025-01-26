6f8e45fabb2770b391ecabe31e5c6b9a4e96ccaa
Bobby <rhulzing@yahoo.com>
2025-01-26 11:22:47 -0600

    animal refactorings
    moved the vision collision to the individual beasts
    tweaked the vision size so they can notice you from further away
    skittish beast runs farther away.

--- SkittishBeast/skittish_beast.gd
+++ SkittishBeast/skittish_beast.gd
@@ -25,29 +25,29 @@ func _physics_process(delta: float) -> void:
 		skittish_behavior.process_behavior(delta) # do defense behavior stuff
 
 
-func _on_bubble_animal_notice_player(player: Player) -> void:
-	# do the collisioning
-	if bubble_animal.get_parent() == default_behavior:
-		skittish_behavior.player = player
-		# skittish_behavior.distance_to_run = distance_to_run_away
-		
-		bubble_animal.move_speed = 8
-		
-		var displacement: Vector3 = bubble_animal.global_position - player.global_position
-		displacement.normalized()
-		var random_angle := rng.randf_range(-0.349066, 0.349066)
-		displacement.rotated(Vector3.UP, random_angle)
-		
-		displacement *= distance_to_run_away
-		displacement += bubble_animal.global_position
-		
-		var destination := Vector3(displacement.x, bubble_animal.global_position.y, displacement.z)
-		skittish_behavior.destination = destination
-		
-		bubble_animal.navigation_agent.target_position = destination
-		
-		bubble_animal.reparent(skittish_behavior)
-
-
 func _on_skittish_behavior_finished_fleeing() -> void:
 	bubble_animal.reparent(default_behavior)
+	
+	
+func _on_vision_collision_entered(body: Node3D) -> void:
+	if body is Player:
+		if bubble_animal.get_parent() == default_behavior:
+			skittish_behavior.player = body
+			# skittish_behavior.distance_to_run = distance_to_run_away
+			
+			bubble_animal.move_speed = 8
+			
+			var displacement: Vector3 = bubble_animal.global_position - body.global_position
+			displacement.normalized()
+			var random_angle := rng.randf_range(-0.349066, 0.349066)
+			displacement.rotated(Vector3.UP, random_angle)
+			
+			displacement *= distance_to_run_away
+			displacement += bubble_animal.global_position
+			
+			var destination := Vector3(displacement.x, bubble_animal.global_position.y, displacement.z)
+			skittish_behavior.destination = destination
+			
+			bubble_animal.navigation_agent.target_position = destination
+			
+			bubble_animal.reparent(skittish_behavior)
