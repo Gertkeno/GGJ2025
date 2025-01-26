67bb20b6c4ca19ca6eb1e8657b22266ff38bea8a
Bobby <rhulzing@yahoo.com>
2025-01-26 12:02:56 -0600

    crouching mechanic now works with animals

--- SkittishBeast/skittish_behavior.gd
+++ SkittishBeast/skittish_behavior.gd
@@ -3,6 +3,7 @@ class_name SkittishBehavior
 
 var player: Player = null
 var bubble_animal: BubbleAnimal = null
+var move_speed = 8
 var destination: Vector3
 
 
@@ -13,6 +14,7 @@ func process_behavior(delta: float) -> void:
 	if is_nav_finished():
 		finished_fleeing.emit()
 	else:
+		bubble_animal.move_speed = move_speed
 		bubble_animal.direction = bubble_animal.global_position.direction_to(destination)
 		bubble_animal.look_at(destination)
 		bubble_animal.apply_movement(delta)
